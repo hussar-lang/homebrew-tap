eb4bc2e10d4f54940b4adb562e333b5145c2e241
kscarlett <kellen@kscarlett.com>
2020-02-01 16:48:26 +0000

    fix: manually fix syntax (#1)

--- Formula/hussar.rb
+++ Formula/hussar.rb
@@ -24,20 +24,11 @@ class Hussar < Formula
     end
   end
   
-  head "https://github.com/hussar-lang/hussar.git"
-  ...
-
   def install
     bin.install "hussar"
   end
 
-  def caveats; <<~EOS
-    How to use this binary
-  EOS
-  end
-
   test do
     system "#{bin}/hussar env"
-    ...
   end
 end
